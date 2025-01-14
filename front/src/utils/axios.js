3cd99630ff09f49e81048317ed8d9bd16044a1e4
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2025-01-14 11:10:04 -0300

    fix url

--- front/src/utils/axios.js
+++ front/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { SERVER } from '../constants';
 
 const clientAxios = axios.create({
-    baseURL: `${SERVER}`
+    baseURL: `${SERVER}api`
 });
 
 export default clientAxios;
